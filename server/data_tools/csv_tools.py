81d9c0a51f724d2c21c9a8230a08c7cf103c8bb0
tbarlow12 <tanner.barlow12@gmail.com>
2017-09-09 04:08:42 -0600

    identify categorical columns

--- server/data_tools/csv_tools.py
+++ server/data_tools/csv_tools.py
@@ -15,7 +15,7 @@ def numeric_labels_features(csv):
         for instance in lines[1:]:
             items = instance.split(',')
             features.append([float(x) for x in items[:-1]])
-            labels.append(float(items[-1]))
+            labels.append(float(items[-1]))        
         return features, labels, titles
 
 def get_data_types(line):
@@ -24,50 +24,47 @@ def get_data_types(line):
         try:
             x = float(item)
             data_types.append('num')
-        except Exception:
+        except ValueError:
             data_types.append('str')
     return data_types
 
-def get_text_indices(lines):
-    indices = set()
-    for line in lines:
-        for i in range(0,len(line)):
-            try:
-                x = float(line[i])
-            except Exception:
-                indices.add(i)
-    return indices
+categorical_limit = 2
 
-def encode_text_features(lines):
-    #encode text stuff
-    for index in text_indices:
-        items = [x[index] for x in lines]
-
+def categorical_or_continuous(lines):
+    result = []
+    for i in range(0,len(lines[0])):
+        distinct_vals = set([line[i] for line in lines])
+        if len(distinct_vals) <= categorical_limit:
+            result.append(0)
+        else:
+            result.append(1)
+    return result
 
 def generic_labels_features(csv):
     with open(csv) as f:
         lines = [line.strip().split(',') for line in f.readlines()]
         titles = lines[0]
         lines = lines[1:]
+
+        category_or_continuous = categorical_or_continuous(lines)
+        
         data_types = get_data_types(lines[0])
         
-        text_indices = get_text_indices(lines)
+        text_indices = get_text_indices(data_types)
 
         pdb.set_trace()
-        
-        if len(text_indices) > 0:
-            encode_text_features(lines)
-            
 
         features = []
         labels = []
-
-
-
         for instance in lines:
             items = instance
             features.append([items[:-1]])
             labels.append(items[-1])
         return features, labels, titles
 
-
+def get_text_indices(data_types):
+    indices = []
+    for i in range(0,len(data_types)):
+        if data_types[i] == 'str':
+            indices.append(i)
+    return indices
